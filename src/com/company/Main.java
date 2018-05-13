6c50d3d42343a65976a2b2952d266e8436e0188c
tahakhany <39022302+tahakhany@users.noreply.github.com>
2018-05-13 16:55:34 +0430

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+import javax.swing.JPanel;
+public class Main
+{
+    public static void main(String[] args)
+    {
+	JPanel myPanel = new JPanel();
+	myPanel.setVisible(true);
+	myPanel.add(myForm);
+    }
+}
